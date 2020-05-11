0a91f2918605d075f5dfd6b0c58946dd7882c498
Gabriel Fonseca <gabriel.fonseca@masp.org.br>
2020-05-11 14:58:57 -0300

    fix mysql bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Menu;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         $menu = new Menu;
         View::share(['menus' => $menu->getMenus()]);
     }
